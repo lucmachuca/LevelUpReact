ddf8afeb4a477126d80dc1c437bf2e57cc076238
Luciano Machuca <luc.machuca@duocuc.cl>
2025-10-28 02:34:03 -0300

    actualizar estilos globales y animaciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import { CarritoProvider } from "./context/CarritoContext";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CarritoProvider>
+      <App />
+    </CarritoProvider>
   </React.StrictMode>
 );
